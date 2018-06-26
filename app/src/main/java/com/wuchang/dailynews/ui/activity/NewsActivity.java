089d42db6c8567f41b7dbba4ba54f827d90ea984
WuChang <18155630531@163.com>
2018-06-26 17:12:51 +0800

    2018/06/26 加载首页Tab

--- app/src/main/java/com/wuchang/dailynews/ui/activity/NewsActivity.java
+++ app/src/main/java/com/wuchang/dailynews/ui/activity/NewsActivity.java
@@ -1,6 +1,8 @@
 package com.wuchang.dailynews.ui.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
@@ -11,6 +13,8 @@ import com.wuchang.dailynews.base.BaseActivity;
 public class NewsActivity extends BaseActivity {
 
     @BindView(R.id.toolbar_news) Toolbar mToolbar;
+    @BindView(R.id.tab_main)
+    TabLayout mTabLayout;
 
     @Override
     public int getLayoutId() {
@@ -24,7 +28,7 @@ public class NewsActivity extends BaseActivity {
 
     @Override
     public void initViews(Bundle savedInstanceState) {
-
+        initTab();
     }
 
     @Override
@@ -34,4 +38,13 @@ public class NewsActivity extends BaseActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 
+    private void initTab() {
+        mTabLayout.setTabMode(TabLayout.MODE_FIXED);
+        mTabLayout.setSelectedTabIndicatorHeight(0);
+        ViewCompat.setElevation(mTabLayout, 10);
+        String[] tabTitles = getResources().getStringArray(R.array.tab_choice);
+        for (int i = 0; i < tabTitles.length; i++) {
+            mTabLayout.addTab(mTabLayout.newTab().setText(tabTitles[i]));
+        }
+    }
 }
